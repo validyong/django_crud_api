0433c0783d574b215269bbf40b65629ff14a8750
validyong <rokudynamics@gmail.com>
2021-03-16 18:23:19 +0900

    add jwt auth

--- django_crud_api/urls.py
+++ django_crud_api/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls import url, include
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 urlpatterns = [
     url(r'^', include('books.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
